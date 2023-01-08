84bcde51a873b5500ae69a2b34a7a9df399925eb
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2023-01-08 02:37:36 +0100

    Delete src directory

--- src/components/Column/index.js
+++
@@ -1,9 +0,0 @@
-import ColumnFormModal from './ColumnFormModal';
-import Column from './Column';
-
-export { default } from './Column';
-
-export {
-	ColumnFormModal,
-	Column
-};
--- src/components/Issue/index.js
+++
@@ -1,9 +0,0 @@
-import IssueFormModal from './IssueFormModal';
-import Issue from './Issue';
-
-export { default } from './Issue';
-
-export {
-	IssueFormModal,
-	Issue
-};
