03e292399a8081c5b7f5902019b77f5a710316b1
LukasSpies <lspies100@gmail.com>
2024-01-25 15:59:35 +0100

    simplify

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,28 +1,51 @@
-import {
-  Row,
-  Col,
-  Card,
-  Statistic,
-  Button,
-  List,
-  Descriptions,
-  Avatar,
-} from "antd";
+import React, { useEffect, useState, useRef } from 'react';
+import ChatBody from '../components/chat/client/src/components/ChatBody';
+import ChatFooter from '../components/chat/client/src/components/ChatFooter';
+import '../components/chat/client/src/index.css';
 
-import { PlusOutlined, ExclamationOutlined } from "@ant-design/icons";
-import mastercard from "../assets/images/mastercard-logo.png";
-import paypal from "../assets/images/paypal-logo-2.png";
-import visa from "../assets/images/visa-logo.png";
-import Board from "../components/Board";
+const Chat = ({ socket }) => {
 
-function Chat() {
-  const data = [
-  ];
+    const [messages, setMessages] = useState([
+        { id: 1, name: "User1", text: "Hello" },
+        { id: 2, name: "User2", text: "Hi there!" },
+    ]);
 
-  return (
-      <>
-      </>
-  );
-}
+
+    const [message, setMessage] = useState('');
+
+
+    const lastMessageRef = useRef(null);
+
+    useEffect(() => {
+        if (socket) {
+            socket.on('message', (message) => {
+                setMessages((prevMessages) => [...prevMessages, message]);
+            });
+
+            return () => {
+                socket.off('message');
+            };
+        }
+    }, [socket]);
+
+    const handleSendMessage = (e) => {
+        e.preventDefault()
+            socket.emit("message",
+                {
+                    text: message,
+                    name: localStorage.getItem("userName"),
+                    id: `${socket.id}${Math.random()}`,
+                    socketID: socket.id
+                }
+            )
+        setMessage("")
+    }
+    return (
+        <div className="chat-container">
+            <ChatBody messages={messages} lastMessageRef={lastMessageRef} />
+            <ChatFooter onSubmit={handleSendMessage} />
+        </div>
+    );
+};
 
 export default Chat;
