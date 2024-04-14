a644011a736f24e8ccecd7d5b1c8aa53630d10de
jonasbender <jonas.bender@tum.de>
2024-04-15 00:27:52 +0200

    add calendar updates

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -14,7 +14,7 @@ import { PlusOutlined, ExclamationOutlined } from "@ant-design/icons";
 import mastercard from "../assets/images/mastercard-logo.png";
 import paypal from "../assets/images/paypal-logo-2.png";
 import visa from "../assets/images/visa-logo.png";
-import Board from "../components/Board";
+
 import React from 'react';
 function Calendar() {
 
