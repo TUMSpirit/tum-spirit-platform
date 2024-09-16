b389fb1ad2370307e93f780ebdf00138ff56d4c4
jonasbender <jonas.bender@tum.de>
2024-09-16 15:38:24 +0200

    update push notifications

--- frontend/src/pages/Kanban.js
+++ frontend/src/pages/Kanban.js
@@ -1,19 +1,4 @@
 import React from 'react';
-import {
-    Row,
-    Col,
-    Card,
-    Statistic,
-    Button,
-    List,
-    Descriptions,
-    Avatar,
-} from "antd";
-
-import { PlusOutlined, ExclamationOutlined } from "@ant-design/icons";
-import mastercard from "../assets/images/mastercard-logo.png";
-import paypal from "../assets/images/paypal-logo-2.png";
-import visa from "../assets/images/visa-logo.png";
 import Home from "../components/Kanban/Home";
 
 function Kanban() {
