93734ab104cc0f7fb377f451fcc1f2b79148d48c
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2024-09-18 01:50:39 +0200

    Update SocketProvider.js

--- frontend/src/context/SocketProvider.js
+++ frontend/src/context/SocketProvider.js
@@ -6,14 +6,20 @@ import { Spin } from 'antd';
 import TKIForm from "../components/TKI/TKIForm.js"
 import { useNavigate } from 'react-router-dom';
 import { useUnreadMessage } from './UnreadMessageContext';
-import ghost from '../assets/images/ios_180x180.png';
+import useNotificationPermission from './NotificationPermission';
+
 
 const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
-  const { getUnreadMessages, incrementNotifications, setUnreadMessages } = useUnreadMessage();
+  const { getUnreadMessages, incrementNotifications, markAsRead, setLastVisited, unreadMessages, setUnreadMessages } = useUnreadMessage();
+  const [missedMessages, setMissedMessages] = useState(0);
   const [currentUser, setCurrentUser] = useState(null);
+  const [userSettings, setUserSettings] = useState(null);
+  const [projectInformation, setProjectInformation] = useState([]);
+  //const [currentTab, setCurrentTab] = useState(null);
+  const [onlineStatus, setOnlineStatus] = useState(null);
   const [loading, setLoading] = useState(true);
   const isAuthenticated = useIsAuthenticated();
   const authHeader = useAuthHeader();
@@ -21,15 +27,24 @@ export const SocketProvider = ({ children }) => {
   const navigate = useNavigate();
   const [isModalOpen, setModalOpen] = useState(false);
 
-  const fetchCurrentUser = async () => {
+  //useNotificationPermission();
+
+  const fetchCurrentUser = async (socketInstance) => {
     try {
       const response = await axios.get('/api/me', {
         headers: {
           "Authorization": authHeader()
         }
       });
+      const { team_id, username, missed_messages_chat } = response.data;
+      console.log(response.data);
       setCurrentUser(response.data);
-      setLoading(false);
+      socketInstance.connect();
+      socketInstance.auth = { teamId: team_id };
+      socketInstance.emit('joinTeam', team_id);
+      socketInstance.emit('userOnline', { team_id, username });
+      // Notify server that user is online
+      // Set loading to false after fetching user and connecting socket
     } catch (error) {
       console.error('Failed to fetch current user:', error);
       logout();
@@ -38,77 +53,173 @@ export const SocketProvider = ({ children }) => {
     }
   };
 
-  const pullUnreadMessages = async () => {
+  const fetchUserSettings = async () => {
     try {
-      const response = await axios.get("/api/missed-chats", {
+      const response = await axios.get('/api/get-settings', {
         headers: {
           "Authorization": authHeader()
         }
       });
-      setUnreadMessages(response.data);
+      setUserSettings(response.data);
+      // If the TKI test should be triggered
+      if (response.data.is_first_login) {
+        updateSettings('is_first_login', false);
+        navigate("/intro");
+      }
+      // Set the TKI test state, but don't open the modal here
+      if (!response.data.is_first_login && response.data.trigger_tki_test) {
+        setModalOpen(true);
+      }
     } catch (error) {
-      console.error('Failed to fetch missed chats:', error);
+      console.error('Failed to fetch current user:', error);
+      logout();
+      navigate("/login");
+      setLoading(false);
     }
   };
 
-  const handleNewMessageNotification = (data) => {
-    incrementNotifications(data.privateChatId || 'Team');
-
-    if (currentUser && data.senderId !== currentUser.username) {
-      const title = "New Message!";
-      const options = {
-        body: `${data.senderId} sent a message in the chat`,
-        icon: ghost,
-        vibrate: [300, 100, 400],
-      };
-      navigator.serviceWorker.ready.then(async function (serviceWorker) {
-        await serviceWorker.showNotification(title, options);
+  const fetchProject = async () => {
+    try {
+      const response = await axios.get(`/api/get-project-by-teamid`, {
+        headers: {
+          "Authorization": authHeader()
+        }
       });
+      setProjectInformation(response.data);
+    } catch (error) {
+      console.error('milestones:', error);
+      logout();
+      navigate("/login");
+      setLoading(false);
+    }
+  };
+
+
+  const updateSettings = async (field, value) => {
+    try {
+      const response = await axios.post('/api/update-settings',
+        {
+          [field]: value  // Dynamically setting the field and its value
+        },
+        {
+          headers: {
+            "Authorization": authHeader()
+          }
+        }
+      );
+
+      console.log(`User settings updated: ${field} set to`, response.data);
+    } catch (error) {
+      console.error('Error updating user settings:', error);
+    }
+  };
+
+
+  const updateLastLoggedIn = async (lastActiveChat) => {
+    console.log(lastActiveChat);
+    try {
+      const response = await axios.post('/api/update-last-active', { "chatData": lastActiveChat }, {
+        headers: {
+          "Authorization": authHeader()
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Failed to update last log in', error);
+    }
+  };
+
+
+  const pullUnreadMessages = async () => {
+    try {
+      const response = await axios.get(
+        "/api/missed-chats", {
+        headers: {
+          "Authorization": authHeader()
+        }
+      }
+      );
+      setUnreadMessages(response.data);
+    } catch (error) {
+      console.error('Failed to update last log in', error);
+    }
+  };
+
+
+  const closeModal = async () => {
+    setModalOpen(false);
+  }
+
+  const handleVisibilityChange = () => {
+    if (document.visibilityState === 'visible') {
+      // Reconnect the socket when the app becomes visible
+      window.location.reload(true);
     }
   };
 
   useEffect(() => {
     if (isAuthenticated) {
-      fetchCurrentUser();
-      pullUnreadMessages();
-    }
-  }, []);
-
-  // New useEffect for socket connection once currentUser is set
-  useEffect(() => {
-    if (currentUser && isAuthenticated) {
       const token = authHeader().split(' ')[1]; // Extract the token from "Bearer <token>"
       const socketInstance = io('https://spirit.lfe.ed.tum.de/', {
         auth: {
           token: token
         },
         transports: ['websocket'],
-        autoConnect: false,
+        autoConnect: false // Prevent auto connection
       });
 
-      socketInstance.connect();
-      socketInstance.emit('joinTeam', currentUser.team_id);
-      socketInstance.emit('userOnline', { team_id: currentUser.team_id, username: currentUser.username });
+      document.addEventListener('visibilitychange', handleVisibilityChange);
 
-      socketInstance.on('newMessageMetadata', handleNewMessageNotification);
+      socketInstance.on('newMessageMetadata', (data) => {
+
+        //console.log('Message received:', data);
+        const chatId = data.privateChatId ? data.privateChatId : 'Team';
+        // Increment notifications
+        incrementNotifications(chatId);
+        //showNotification(); 
+      });
 
       socketInstance.on('updateUserStatus', ({ data }) => {
-        // Handle online status
-        console.log('User status updated', data);
+        for (const username in data) {
+          data[username] = 'online';
+        }
+        setOnlineStatus(data);
+        setLoading(false);
+      });
+
+
+
+      socketInstance.on('userDisconnected', () => {
+      });
+
+      socketInstance.on('connect', () => {
+        console.log('Connected to WebSocket');
       });
 
       socketInstance.on('disconnect', () => {
         console.log('Disconnected from WebSocket');
+        //updateLastLoggedIn(unreadMessages);
       });
 
+
+      pullUnreadMessages();
+      fetchCurrentUser(socketInstance);
+      fetchUserSettings();
+      fetchProject();
       setSocket(socketInstance);
 
       return () => {
-        socketInstance.off('newMessageMetadata', handleNewMessageNotification);
-        socketInstance.disconnect();
+        socketInstance.off('updateUserStatus');
+        socketInstance.off('newMessageMetadata');
+        //updateLastLoggedIn(unreadMessages);
+        //socketInstance.disconnect();
       };
+    } else {
+      logout();
+      navigate("/login");
+      setLoading(false);
     }
-  }, [currentUser]);
+  }, []);
 
   if (loading) {
     return (
@@ -119,9 +230,9 @@ export const SocketProvider = ({ children }) => {
   }
 
   return (
-    <SocketContext.Provider value={{ currentUser, socket }}>
+    <SocketContext.Provider value={{ currentUser, onlineStatus, socket, missedMessages, projectInformation, updateLastLoggedIn, updateSettings }}>
       {children}
-      <TKIForm visible={isModalOpen} onClose={() => setModalOpen(false)} />
+      <TKIForm visible={isModalOpen} onClose={closeModal} />
     </SocketContext.Provider>
   );
 };
