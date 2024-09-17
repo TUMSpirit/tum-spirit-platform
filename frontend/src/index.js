a087d06860bc7c740191158ad3422bc522c8fa66
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2024-09-17 22:49:37 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-//import axios from "axios";
+import axios from "axios";
 
 import { BrowserRouter } from "react-router-dom";
 
