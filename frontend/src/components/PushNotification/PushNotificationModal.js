34aa2e4f9f6caba5826422f2fdeeb1358b02a65f
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2024-09-18 13:21:34 +0200

    Update PushNotificationModal.js

--- frontend/src/components/PushNotification/PushNotificationModal.js
+++ frontend/src/components/PushNotification/PushNotificationModal.js
@@ -116,23 +116,6 @@ const PushNotificationModal = ({ modalIsOpen, setModalIsOpen }) => {
     }
   };
 
-  // Helper function to convert VAPID key from base64 to Uint8Array
-  function urlBase64ToUint8Array(base64String) {
-      const padding = '='.repeat((4 - base64String.length % 4) % 4);
-      const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-      const rawData = window.atob(base64);
-      const outputArray = new Uint8Array(rawData.length);
-      for (let i = 0; i < rawData.length; ++i) {
-          outputArray[i] = rawData.charCodeAt(i);
-      }
-      return outputArray;
-  }
-
-  const displaySubscriptionInfo = (subscription) => {
-    setSubscription(subscription);
-    console.log('Active subscription:', subscription);
-  };
-
   /*const testSend = () => {
     const title = "Push title";
     const options = {
