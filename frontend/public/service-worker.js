cb310fefc1230c1aa49743eb3d1155e48d151ac3
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2024-09-19 00:33:35 +0000

    Update service-worker.js

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -12,6 +12,20 @@ self.addEventListener('push', function(event) {
     );
 });
 
+self.addEventListener('message', function(event) {
+    const data = event.data ? event.data.json() : {};
+
+    const options = {
+        body: data.body || 'You have a new notification!',
+        icon: data.icon || '/icon.png',
+        data: { url: data.url || '/' }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title || 'New Notification!', options)
+    );
+});
+
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
     if (!event.notification.data || !event.notification.data.url) {
