749e7906d72ba89ea56d8e2e6ff3f4099c1537e4
Jonas Bender <33391386+derGoldeneJonas@users.noreply.github.com>
2024-09-19 16:55:09 +0200

    Update SocketProvider.js

--- frontend/src/context/SocketProvider.js
+++ frontend/src/context/SocketProvider.js
@@ -226,22 +226,6 @@ export const SocketProvider = ({ children }) => {
         // Increment notifications
         incrementNotifications(chatId);
         //showNotification();         
-        axios.post('api/send-notification', {
-              username: data.senderId,   // Assuming currentUser contains the logged-in user
-              message: data.content  // Example message
-          },{
-        headers: {
-          "Authorization": authHeader()
-          }
-        })
-          .then(response => {
-            console.log(response.data.message);  // You can show success message or handle response
-          })
-          .catch(error => {
-            console.error('Error triggering notification:', error);
-        });
-
-      });
 
       socketInstance.on('updateUserStatus', ({ data }) => {
         for (const username in data) {
